482f8030f8e7d03cccc6a91b465d869104655cd4
Aadi Prakash <aadiaju4319@gmail.com>
2024-10-14 14:42:17 +0530

    Vite upgraded

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/Seller/',
   plugins: [react()],
   optimizeDeps: {
     include: ['recharts'],  // Force Vite to optimize recharts on startup
